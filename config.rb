abe774d2d99e9f7a0d76e6ea2d03084fefa8b00c
Louise Darche <louise.darche@gmail.com>
2018-05-08 10:57:29 +0200

    didnot finished the layout of flats

--- config.rb
+++ config.rb
@@ -20,3 +20,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+users = data.flats.keys
+users.each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
